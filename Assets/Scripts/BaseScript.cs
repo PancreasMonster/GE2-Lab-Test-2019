dd7d1b1a3af3071f19349a939c249a3cb077f6e2
PancreasMonster <ryanflood444@gmail.com>
2019-03-26 10:39:57 +0000

    Enemies Arrive, Stop And Fire

--- Assets/Scripts/BaseScript.cs
+++ Assets/Scripts/BaseScript.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class BaseScript : MonoBehaviour {
 
+    public List<GameObject> bases = new List<GameObject>();
     public float tiberium = 5;
     bool refresh = false;
     TextMesh text;
@@ -21,10 +22,10 @@ public class BaseScript : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        text.text = tiberium.ToString();
         if (!refresh)
         {
             tiberium += 1;
-            text.text = tiberium.ToString();
             refresh = true;
             StartCoroutine(tiberiumAdd());
         }
@@ -34,6 +35,19 @@ public class BaseScript : MonoBehaviour {
             tiberium -= 10;
             GameObject clone = Instantiate(fighterPrefab, transform.position, Quaternion.identity);
             clone.GetComponent<Renderer>().material.color = GetComponent<Renderer>().material.color;
+            int rand = Random.Range(0, bases.Count - 1);
+            clone.GetComponent<Arrive>().targetPosition = bases[rand].GetComponent<Transform>().position;
+            clone.GetComponent<Arrive>().slowingDistance = 15;
+            clone.GetComponent<FighterTiberium>().AssignTarget(bases[rand], this.gameObject);
+        }
+    }
+
+    void OnTriggerEnter (Collider col)
+    {
+        if(col.transform.tag == "bullet")
+        {
+            Destroy(col.transform.gameObject);
+            tiberium -= .5f;
         }
     }
 
---
+++ Assets/Scripts/FighterTiberium.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FighterTiberium : MonoBehaviour {
+
+    TextMesh text;
+    public float fireRate = .4f, force = 500;
+    bool bulletCooldown, refueling, attacking;
+    public float tiberium = 7;
+    GameObject targetBase, homeBase;
+    public GameObject bullet;
+    Arrive arrive;
+    Boid boid;
+
+    // Use this for initialization
+    void Start () {
+        text = GetComponentInChildren<TextMesh>();
+        arrive = GetComponent<Arrive>();
+        boid = GetComponent<Boid>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        text.text = tiberium.ToString();
+
+        if (Vector3.Distance(transform.position, targetBase.transform.position) < 15 && !refueling)
+        {
+            arrive.weight = 0;
+            boid.velocity = Vector3.zero;
+            if (!bulletCooldown)
+                FireBullet();
+        }
+
+        if (tiberium <= 0)
+        {
+            arrive.weight = 1;
+            arrive.targetGameObject = homeBase;
+            attacking = false;
+            refueling = true;
+        }
+	}
+
+    public void AssignTarget (GameObject target, GameObject home)
+    {
+        targetBase = target;
+        homeBase = home;
+    }
+
+    void FireBullet ()
+    {
+        tiberium -= 1;
+        Vector3 dir = targetBase.transform.position - transform.position;
+        dir.Normalize();
+        GameObject clone = Instantiate(bullet, transform.position, Quaternion.identity);
+        clone.GetComponent<Renderer>().material.color = GetComponent<Renderer>().material.color;
+        Rigidbody rb = clone.GetComponent<Rigidbody>();
+        rb.AddForce(dir * force);
+        rb.useGravity = false;
+        bulletCooldown = true;
+        StartCoroutine(BulletCooldownFunction());
+    }
+
+    IEnumerator BulletCooldownFunction ()
+    {
+        yield return new WaitForSeconds(fireRate);
+        bulletCooldown = false;
+    }
+}
